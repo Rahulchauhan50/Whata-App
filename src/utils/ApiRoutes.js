9a9890b53fb4a7d88408f49dd7cda53c1fbf4d18
Rahul chauhan <rahul@Rahuls-MacBook-Pro.local>
2024-02-15 10:35:05 +0530

    15 feb

--- src/utils/ApiRoutes.js
+++ src/utils/ApiRoutes.js
@@ -1,4 +1,4 @@
-export const HOST = 'http://localhost:5000'
+export const HOST = 'https://whatsapp-p2yq.onrender.com'
 
 const AUTH_ROUTE = `${HOST}/api/auth`
 const MESSAGES_ROUTE = `${HOST}/api/messages`
